cfec3364421e7606e85cc1af90ab03c6b7332258
anap <anapcwb@gmail.com>
2019-10-20 20:11:20 +0100

    add a state to the component

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,34 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../App.css';
-import Lamp from './Lamp';
-
-//import Quote from './Quote';
-import Quotes from './Quotes';
+import React, { Component } from "react";
+import logo from "../logo.svg";
+import "../App.css";
+//import Lamp from "./Lamp";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      working: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ working: !this.state.working });
+  };
+
   render() {
+    const isWorking = this.state.working ? "Break" : "Work";
+    const rotate = this.state.working ? "rotate" : "";
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Simpsons Quotes</h1>
+          <img src={logo} className={`App-logo ${rotate}`} alt="logo" />
+          <button onClick={this.handleClick}>{isWorking}</button>
         </header>
-
-        <Lamp on />
-        <Lamp />
-        <Quotes />
       </div>
     );
   }
 }
 
 export default App;
+
--- src/components/Lamp.js
+++ src/components/Lamp.js
@@ -1,18 +1,23 @@
 // Lamp.js
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Lamp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      on: false
+      on: this.props.on
     };
   }
+
+  handleClick = () => {
+    this.setState({ on: !this.state.on });
+  };
+
   render() {
-    const light = this.state.on ? 'on' : 'off';
+    const light = this.state.on ? "on" : "off";
     return (
       <div className="Lamp">
-        <button onClick={() => alert('Button clicked')} className={light}>
+        <button onClick={this.handleClick} className={light}>
           {light.toUpperCase()}
         </button>
         <figure className={light} />
